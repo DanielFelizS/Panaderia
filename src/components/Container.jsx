9ff585d6e334a3b8af695ebc540fc64cb12709ad
DANIEL  FELIZ  SANTANA <a19a05048@educacion.edu.do>
2024-02-15 13:43:47 -0400

    Modificando rutas de imagenes 2.1

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -4,7 +4,7 @@ export const Container = ({ imgUrl, name, precio }) => {
   return (
     <div className="contenedor">
       <figure>
-        <img src={`/img/${imgUrl}`} alt={name} />
+        <img src={`/public/img/${imgUrl}`} alt={name} />
         <div className="cap">
           <h3>{name}</h3>
           <p>Precio: RD${precio}</p>
